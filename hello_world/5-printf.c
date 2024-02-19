71574ff07da1f963315678f6bab0bdb42a41f580
AstronautSP <victorlafond33@gmail.com>
2024-02-19 14:40:06 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,20 +1,13 @@
-/*
- * * File: 5-printf.c
- * * Auth: Brennan D Baraba
- */
-
-
 #include <stdio.h>
 
 /**
- * * main - Prints "with proper grammar, but the outcome is
- * 			a piece of art," followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
  *
- *Return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
+
 	return (0);
-]
