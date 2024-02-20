f66097616bb66bef393d92d120894dc95e11ed8a
AstronautSP <victorlafond33@gmail.com>
2024-02-20 10:32:59 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,10 @@
-/*
+/**
  * File: 6-size.c
  * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints the size of various types based on
@@ -17,4 +18,7 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
