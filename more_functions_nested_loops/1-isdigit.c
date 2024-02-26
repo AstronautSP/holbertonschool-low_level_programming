6f979579c357b7f60638ea046336dfc3b6c964e9
AstronautSP <victorlafond33@gmail.com>
2024-02-26 15:34:13 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - function that verifies if a character is a digit or not
  * @c: tested character
- * Return returns 1 if is uppercase , O if not
+ * Return: returns 1 if is uppercase , O if not
  */
 
 int _isdigit(int c)
