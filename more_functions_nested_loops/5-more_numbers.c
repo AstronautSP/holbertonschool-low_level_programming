a21674b133c1d99633db40830c15ff08ac09e554
AstronautSP <victorlafond33@gmail.com>
2024-02-28 08:51:43 +0000

    5-more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,24 @@
 #include "main.h"
+
 /**
-*more_numbers - prints number to 14
-*
-*/
+ * more_numbers - prints number to 14
+ */
+
 void more_numbers(void)
 {
-int number 1, number 2
+int number1, number2;
 
-for ( number1 =0; number 1 < 10 ; number1++)
-}
-for ( number2 =0; number 1 <= 14 ; number2++)
-
+for (number1 =0 ; number1 < 10 ; number1++)
+{	
+for (number2 =0 ; number1 <= 14 ; number2++)
 {
-if (number2 >9)
+
+if (number2 > 9)
 {
 _putchar((number2 / 10) + '0');
 }
 _putchar((number2 % 10) + '0');
 }
 _putchar(10);
+} 
 }
-}²
