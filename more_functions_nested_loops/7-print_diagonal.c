265b8b6cd53e5687327300964520ae7aa4c7a3e1
AstronautSP <victorlafond33@gmail.com>
2024-02-28 13:37:39 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,14 +13,15 @@ if (n > 0)
 for (len = 0; len < n; len++)
 {
 for (space = 0; space < len; space++)
+{
 _putchar(' ');
-
+}
 _putchar('\\');
-
-if (len == (n - 1))
-
 _putchar('\n');
 }
 }
+else
+{
 _putchar('\n');
+}
 }
