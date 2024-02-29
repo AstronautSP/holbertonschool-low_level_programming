14246f8f24b7231ae847ae2a57afa662ac01590f
AstronautSP <victorlafond33@gmail.com>
2024-02-29 14:06:51 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
  *]s:string to print
  */
 
-void print_rev(char*s)
+void print_rev(chars *)
 {
 int len = 0, index = 0;
 len = _strlen(s);
@@ -24,7 +24,7 @@ int _strlen(char *s)
 {
 int count, inc;
 inc = 0;
-for (count = 0; s[count] != '\0'; count++) 
+for (count = 0; s[count] != '\0'; count++)
 inc++;
 
 return (inc);
