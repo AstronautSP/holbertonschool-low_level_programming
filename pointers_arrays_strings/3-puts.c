a158a8295bf54b0efbf3e5cf8f5837dac898b7a9
AstronautSP <victorlafond33@gmail.com>
2024-02-29 10:40:31 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
-*_puts - prints a string
+ * _puts - prints a string
  * @str: string to print
- *
+ * _putchar:return
  * Description: prints a string
+ *
  * On success: return the number of characters printed
  */
 
 void _puts(char *str)
 {
 while (*str)
-_putchar(*str++*);
-
-_putchar('\n');
+	_putchar(*str++);
+	return(0)
+	
 }
+
